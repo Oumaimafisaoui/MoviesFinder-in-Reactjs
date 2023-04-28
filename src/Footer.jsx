63516d727571db9c5460b9c997243b29f7086b76
Oumaima Fisaoui <oufisaou@e2r9p14.1337.ma>
2023-04-28 15:09:56 +0100

    setup

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Footer = () =>
+{
+    return (
+        <div className="footer">
+        <p>Developed by <a href="https://github.com/Oumaimafisaoui">Oumaima Fisaoui</a></p>
+      </div>
+    )
+   
+}
+
+export default Footer;
